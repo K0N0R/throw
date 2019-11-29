13c8c11cc17a5f19bc344a80b7f84e40473ba3ba
k0n0r <maciejdrzewickimd@gmail.com>
2019-11-29 01:13:26 +0100

    serverconfig fix

--- src/shared/serverConfig.ts
+++ src/shared/serverConfig.ts
@@ -1,2 +1,3 @@
-export const host = '51.83.128.137';
+const ENV = process.argv.find((arg) => arg.includes('dist')) ? 'production' : 'development';
+export const host = (ENV === 'production' ? '51.83.128.137' : 'localhost');
 export const port = 8080;
