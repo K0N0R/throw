29cfb47cf29e86ba01a676cac4007c98fc52dc56
Maciej Drzewicki <mdrzewicki@insert-ad.insert.intranet>
2020-06-15 07:51:44 +0200

    accesibility fix

--- src/client/view/game-page.tsx
+++ src/client/view/game-page.tsx
@@ -23,16 +23,23 @@ export default class GamePage extends Component {
 
     render() {
         return [
-            <div class="score">
-                <div class="score-value">
-                    <div class="team-cube team-cube--left"></div>
-                    <div id="score-left">0</div>
+            <div>
+                <div class="score">
+                    <div class="score-value">
+                        <div class="team-cube team-cube--left"></div>
+                        <div id="score-left">0</div>
+                    </div>
+                    <div>-</div>
+                    <div class="score-value">
+                        
+                        <div id="score-right">0</div>
+                        <div class="team-cube team-cube--right"></div>
+                    </div>
                 </div>
-                <div>-</div>
-                <div class="score-value">
-                    
-                    <div id="score-right">0</div>
-                    <div class="team-cube team-cube--right"></div>
+                <div class="configuration">
+                    <div>X - shoot</div>
+                    <div>SHIFT - dash</div>
+                    <div> ARROW KEYS - movement</div>
                 </div>
             </div>
         ];
--- src/shared/serverConfig.ts
+++ src/shared/serverConfig.ts
@@ -1,3 +1,3 @@
 const ENV = process.argv.find((arg) => arg.includes('dist')) ? 'production' : 'development';
-export const host = (ENV === 'production' ? '51.83.128.137' : '192.168.100.63'); // localhost
+export const host = (ENV === 'production' ? '51.83.128.137' : '10.1.0.78'); // localhost // work 10.1.0.78
 export const port = 8080;
