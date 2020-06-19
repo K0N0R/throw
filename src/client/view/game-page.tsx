fe52bc547f798d56816657f87a17ce7f9c53af24
macdes7 <maciej.deskiewicz@gmail.com>
2020-06-19 22:25:02 +0200

    sroom name ;)

--- src/client/view/game-page.tsx
+++ src/client/view/game-page.tsx
@@ -6,7 +6,7 @@ import { KeysHandler } from './../../shared/keysHandler'
 import { game_config } from './../../shared/callibration';
 
 
-export default class GamePage extends Component {
+export default function GamePage() {
 
     componentDidMount() {
         const game = new Game();
@@ -21,27 +21,25 @@ export default class GamePage extends Component {
     }
     
 
-    render() {
-        return [
-            <div>
-                <div class="score">
-                    <div class="score-value">
-                        <div class="team-cube team-cube--left"></div>
-                        <div id="score-left">0</div>
-                    </div>
-                    <div>-</div>
-                    <div class="score-value">
-                        
-                        <div id="score-right">0</div>
-                        <div class="team-cube team-cube--right"></div>
-                    </div>
+    return (
+        <div>
+            <div class="score">
+                <div class="score-value">
+                    <div class="team-cube team-cube--left"></div>
+                    <div id="score-left">0</div>
                 </div>
-                <div class="configuration">
-                    <div>X - shoot</div>
-                    <div>SHIFT - dash</div>
-                    <div> ARROW KEYS - movement</div>
+                <div>-</div>
+                <div class="score-value">
+                    
+                    <div id="score-right">0</div>
+                    <div class="team-cube team-cube--right"></div>
                 </div>
             </div>
-        ];
-    }
+            <div class="configuration">
+                <div>X - shoot</div>
+                <div>SHIFT - dash</div>
+                <div> ARROW KEYS - movement</div>
+            </div>
+        </div>
+    );
 }
