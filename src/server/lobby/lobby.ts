bed00802aad657a454d2eb1688fbbe7f54376029
k0n0r <maciejdrzewickimd@gmail.com>
2020-06-20 20:55:39 +0200

    backend lobby cleanup

--- src/server/lobby/lobby.ts
+++ src/server/lobby/lobby.ts
@@ -9,46 +9,45 @@ import { Room } from './room';
 
 export class Lobby {
     private lobbyId = uuid();
-    public users: User[] = []
     public rooms: Room[] = [];
-    public io!: io.Server;
-    public constructor(io: io.Server) {
+    public constructor(private io: io.Server) {
         this.initConnection(io);
     }
 
     public initConnection(io: io.Server): void {
-        this.io = io;
         this.io.on('connection', (socket: io.Socket) => {
             socket.on('connection::data', (data: string) => {
                 const parsedData: IConnectionData = JSON.parse(data);
                 const user = new User(socket, parsedData.nick, parsedData.avatar)
-                this.users.push(user);
-                this.manageRoomEvents(user);
+                this.bindUserEvents(user);
             });
         });
     }
 
-    private manageRoomEvents(user: User): void {
-        //#region general
-        user.socket.on('disconnect', () => {
-            this.userLeaveRoom(user);
-        });
-        //#endregion
-
+    private bindUserEvents(user: User): void {
         //#region room
         user.socket.on('room::create', (data: IRoomCreate) => {
-            const room = new Room(this.io, user.socket.id, data.name, data.password, data.maxPlayersAmount);
+            const room = new Room(
+                this.io,
+                user.socket.id,
+                data.name,
+                data.password,
+                data.maxPlayersAmount,
+                () => {
+                    this.io.to(this.lobbyId).emit('lobby::room-list', this.rooms.map(item => item.getData()));
+                },
+                () => {
+                    const idx = this.rooms.indexOf(room);
+                    this.rooms.splice(idx, 1);
+                });
             this.rooms.push(room);
-            this.userJoinRoom(user, room);
+            room.userJoins(user);
             user.socket.emit('room::created', room.getData(true));
-        });
-        user.socket.on('room::leave', (data: IRoomLeave) => {
-            this.userLeaveRoom(user);
         });
         user.socket.on('room::join', (data: IRoomJoin) => {
             const room = this.rooms.find(item => item.id === data.id);
             if (room  && room.password === data.password) {
-                this.userJoinRoom(user, room);
+                room.userJoins(user);
             }
         });
         //#endregion
@@ -56,55 +55,11 @@ export class Lobby {
         //#region lobby
         user.socket.on('lobby::enter', () => {
             user.socket.join(this.lobbyId);
-            user.socket.emit('lobby::room-list', this.rooms.map(item => ({ id: item.id, name: item.name, players: item.users.length})));
+            user.socket.emit('lobby::room-list', this.rooms.map(room => room.getData()));
         });
         user.socket.on('lobby::leave', () => {
             user.socket.leave(this.lobbyId);
         });
         //#endregion
     }
-
-    private userLeaveRoom(user: User): void {
-        const room = this.rooms.find(room => room.users.find(roomUser => roomUser.socket.id === user.socket.id));
-        if (!room) return; // user wasnt in room
-
-        // admin of room leave
-        if (user.socket.id === room.adminId) {
-            room.users.forEach(user => {
-                user.socket.leave(room.id);
-                room.removeUser(user);
-            });
-            const idx = this.rooms.indexOf(room);
-            this.rooms.splice(idx, 1);
-            this.io.to(room.id).emit('room::destroyed');
-        } else { // user of room leave
-            user.socket.leave(room.id);
-            room.removeUser(user);
-        }
-        this.updateLobbyList();
-        this.updateRoom(room);
-    }
-
-    private userJoinRoom(user: User, room: Room): void {
-        room.join(user);
-        this.updateRoom(room);
-        user.socket.emit('room::joined', room.getData(true));
-        this.updateLobbyList();
-
-        user.socket.on('room::update', (lobbyRoom: ILobbyRoom) => {
-            room.update(lobbyRoom)
-            this.updateRoom(room);
-            this.updateLobbyList();
-            room.resetTemporary();
-        });
-    }
-
-    private updateLobbyList(): void {
-        this.io.to(this.lobbyId).emit('lobby::room-list', this.rooms.map(item => item.getData()));
-    }
-
-    private updateRoom(room: Room): void {
-        this.io.to(room.id).emit('room::changed', room.getData(true));
-    }
-    
 }
--- src/server/lobby/room.ts
+++ src/server/lobby/room.ts
@@ -7,7 +7,6 @@ import { Team } from '../../shared/team';
 
 export class Room {
     public id: string;
-    public playing: boolean = false;
     public users: User[] = [];
     public timeLimit = 6;
     public scoreLimit = 10;
@@ -17,7 +16,7 @@ export class Room {
         value: string;
     } | null;
     public newLastMessage = false;
-    private game: Game;
+    private game: Game | null;
     private gameInterval: any;
 
     public constructor(
@@ -25,62 +24,57 @@ export class Room {
         public adminId: string,
         public name: string,
         public password: string,
-        public maxPlayersAmount: number) {
+        public maxPlayersAmount: number,
+        private onUpdate: () => void,
+        private onDestroy: () => void) {
         this.id = uuid();
     }
 
-    public removeUser(user: User): void {
+    public userJoins(user: User): void {
         const idx = this.users.indexOf(user);
-        if (idx !== -1) {
-            this.users.splice(idx,1);
-        }
-    }
+        if (idx !== -1) return;
 
-    public join(user: User) {
         this.users.push(user);
         user.socket.join(this.id);
+
+        this.notifyChange();
+        user.socket.emit('room::joined', this.getData(true));
+        this.onUserJoins(user);
     }
 
-    public resetTemporary(): void {
-        this.lastMessage = null;
+    private onUserJoins(user: User): void {
+        user.socket.on('room::update', (lobbyRoom: ILobbyRoom) => this.update(lobbyRoom, user));
+        user.socket.on('room::leave', () => this.userLeaves(user));
+        user.socket.on('disconnect', () => this.userLeaves(user));
     }
 
-    public update(room: ILobbyRoom): void {
-        if (room?.data?.adminId) {
-            this.adminId = room.data.adminId;
-            this.users.forEach(user => {
-                const userTeam = room?.data?.users.find(item => item.socketId === user.socket.id)?.team;
-                user.team = userTeam ?? Team.Spectator;
-            });
-            this.timeLimit = room.data.timeLimit;
-            this.scoreLimit = room.data.scoreLimit;
-            this.lastMessage = room.data.lastMessage;
-            if (room.playing && !this.playing) {
-                this.startGame();
-            } else if(!room.playing && this.playing) {
-                this.stopGame();
-            }
-            this.playing = room.playing;
+    private userLeaves(user: User): void {
+        if (user.socket.id === this.adminId) { // admin of room leave
+            this.users.forEach(this.kickUser.bind(this));
+            this.io.to(this.id).emit('room::destroyed');
+            this.onDestroy();
+        } else { // user of room leave
+            this.kickUser(user);
         }
+        this.notifyChange();
     }
 
-
-    public startGame(): void {
-        this.game = new Game(this.io, this.users, this.timeLimit, this.scoreLimit, this.id);
-        this.gameInterval = setInterval(() => {
-            this.game.run();
-        }, 0);
+    private kickUser(user: User): void {
+        user.socket.leave(this.id);
+        const idx = this.users.indexOf(user)
+        if (idx !== -1) this.users.splice(idx, 1);
     }
 
-    public stopGame(): void {
-        clearInterval(this.gameInterval);
+    private notifyChange(): void {
+        this.io.to(this.id).emit('room::changed', this.getData(true));
+        this.onUpdate();
     }
 
     public getData(detailed?: boolean): ILobbyRoom {
         const lobbyRoom: ILobbyRoom = {
             id: this.id,
             name: this.name,
-            playing: this.playing,
+            playing: this.game != null,
             players: this.users.length,
         };
         if (detailed) {
@@ -100,4 +94,40 @@ export class Room {
         }
         return lobbyRoom;
     }
+
+    public update(room: ILobbyRoom, user: User): void {
+        if (user.socket.id === this.adminId) {
+           this.adminId = room.data.adminId;
+            this.timeLimit = room.data.timeLimit;
+            this.scoreLimit = room.data.scoreLimit;
+            // react on data change
+            this.users.forEach(user => {
+                const userTeam = room?.data?.users.find(item => item.socketId === user.socket.id)?.team;
+                user.team = userTeam ?? Team.Spectator;
+            });
+            if (room.playing && !this.game) {
+                this.startGame();
+            } else if(!room.playing && this.game) {
+                this.stopGame();
+            }
+        }
+
+        this.lastMessage = room.data.lastMessage;
+        this.notifyChange();
+        // clear temporary data - only for one notify
+        this.lastMessage = null;
+    }
+
+    //#region game
+    public startGame(): void {
+        this.game = new Game(this.io, this.users, this.timeLimit, this.scoreLimit, this.id);
+        this.gameInterval = setInterval(() => {
+            this.game.run();
+        }, 0);
+    }
+
+    public stopGame(): void {
+        clearInterval(this.gameInterval);
+    }
+    //#endregion
 }
