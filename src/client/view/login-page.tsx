6f6c1bda072d15d58ffabe99f692aab317d63c1c
macdes7 <maciej.deskiewicz@gmail.com>
2020-06-26 19:43:33 +0200

    style add

--- src/client/view/login-page.tsx
+++ src/client/view/login-page.tsx
@@ -43,7 +43,7 @@ export default function StartPage() {
             </div>
 
             <button
-                class="form-btn form-btn-submit"
+                class="form-btn form-btn-submit form-btn-submit--primary"
                 onClick={onConfirm}>
                 Throw!
             </button>
