cd77ce9c8fb444aaa9e01b7051b6a7233e1e3f2d
k0n0r <maciejdrzewickimd@gmail.com>
2020-10-13 11:06:31 +0200

    no dash server config fix

--- src/shared/serverConfig.ts
+++ src/shared/serverConfig.ts
@@ -1,2 +1,7 @@
-export const host = 'localhost'; // localhost // work 10.1.0.78
-export const port = 8080;
+import dotenv from 'dotenv';
+import path from 'path'
+
+const result = dotenv.config();
+console.log(path.resolve(process.cwd(), '.env'));
+export const HOST = process.env.HOST ?? 'localhost'; // localhost // work 10.1.0.78
+export const PORT = process.env.PORT ?? 8080;
