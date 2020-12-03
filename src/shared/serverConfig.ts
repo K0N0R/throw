8c4698446e55302a0a9e17c8ecafa68ab9035d99
k0n0r <maciejdrzewickimd@gmail.com>
2020-12-03 12:22:47 +0100

    server config change

--- src/shared/serverConfig.ts
+++ src/shared/serverConfig.ts
@@ -4,4 +4,4 @@ import path from 'path'
 const result = dotenv.config();
 console.log(path.resolve(process.cwd(), '.env'));
 export const HOST = process.env.HOST ?? 'localhost'; // localhost // work 10.1.0.78
-export const PORT = process.env.PORT ?? 8080;
+export const PORT = process.env.PORT ?? 7070;
